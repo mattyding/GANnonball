2b6abc4d949fc2749d7b733f92a9ebb0452288ef
Casey Manning <kcgrind@gmail.com>
2022-04-30 15:07:07 -0700

    implemented preprocessing for note only + data vis

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,9 @@ import mido
 import numpy as np
 
 
-def main():
+segment_time = 30 #
+
+def loadData():
     '''
     get notes from all midi files
     '''
@@ -17,7 +19,6 @@ def main():
             if not msg.is_meta: 
                 num_msgs += 1
     
-    data = np.empty((num_msgs, 5), dtype=object)
     '''
     data[i] = [note_on/note_off, channel, note, velocity, time]
     
@@ -25,25 +26,48 @@ def main():
         mido.Message('note_off', channel=0, note=58, velocity=83, time=276) --> ['off' 0 58 83 276]
     '''
 
-    # TODO: some clustering where notes from the same piece are clustered together?
-    counter = 0
-    for midi_file in midi_files:
-        for msg in mido.MidiFile(midi_file).tracks[0]:  # only one melody track
+    data = np.zeros((len(midi_files), 30000), dtype=float)
+
+    for i in range(len(midi_files)):
+        noteStarts = {}
+        noteEnds = {}
+        currTime = 0
+        for msg in mido.MidiFile(midi_files[i]).tracks[0]:  # only one melody track
             if msg.is_meta: 
-                print(str(msg))
-                continue
-            
-            msg_data = str(msg).split()
-            data[counter][0] = msg_data[0].split('_')[1]
-            data[counter][1] = int(msg_data[1].split('=')[1])
-            data[counter][2] = int(msg_data[2].split('=')[1])
-            data[counter][3] = int(msg_data[3].split('=')[1])
-            data[counter][4] = int(msg_data[4].split('=')[1])
-            counter += 1
-        print(f"finished processing {midi_file}", end='\r')
-    print(data)
+               continue
+
+            type = msg.type
+            note = msg.note
+            velocity = msg.velocity
+            time = msg.time
+            channel = msg.channel
+            currTime += time
+
+            if type == 'note_on':
+                if note not in noteStarts:
+                    noteStarts[note] = []
+                noteStarts[note].append(currTime) #TODO: add velocity as tuple or something
+            if type == 'note_off':
+                if note not in noteEnds:
+                    noteEnds[note] = []
+                noteEnds[note].append(currTime)
+        
+
+        notes = list(noteStarts.keys())
+        for note in notes:
+            starts = [x for x in noteStarts[note]]
+            ends = [x for x in noteEnds[note]]
+
+            starts.sort()
+            ends.sort()
+
+            for j in range(len(starts)):
+                data[i][starts[j]:ends[j]] = note
+
+        print(f"finished processing {midi_files[i]}", end='\r')
+    return data
             
 
 
 if __name__ == '__main__':
-    main()
+    print(loadData())
